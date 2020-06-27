a3fa75417c7d2abadad31cdfc9733f85cff12228
kennygao <kennygao@users.noreply.github.com>
2020-06-27 08:55:36 -0700

    template.py

--- wip/template.py
+++ wip/template.py
@@ -4,17 +4,17 @@ from typing import List
 class Solution:
     # noinspection PyMethodMayBeStatic
     # noinspection PyPep8Naming
-    def solve(self, ns: List[int]) -> int:
-        return solution(ns)
+    def solution(self, ns: List[int]) -> int:
+        return solve(ns)
 
 
-def solution(ns: List[int]):
+def solve(ns: List[int]) -> int:
     return sum(ns)
 
 
 def test():
-    assert Solution().solve(list(range(10))) == 45
-    assert Solution().solve(list(range(100))) == 4950
+    assert Solution().solution(list(range(10))) == 45
+    assert Solution().solution(list(range(100))) == 4950
 
 
 if __name__ == "__main__":
